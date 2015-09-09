0bece2b21254ee26c7fade57d8f303572603eee9
Lennart Jakobsson <len.nart@hotmail.com>
2015-09-09 14:12:10 +0200

    test

---
+++ OOMULab1/src/Grupp4/Calculator/Model/Tokens/Operator.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Grupp4.Calculator.Model.Tokens;
+
+/**
+ *
+ * @author Lennart
+ */
+public class Operator extends Token{
+    
+}
