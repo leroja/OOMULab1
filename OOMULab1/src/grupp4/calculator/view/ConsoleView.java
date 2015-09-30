5b806f55dc181a2fa2ce56f4292f62660a1256f0
Lennart Jakobsson <len.nart@hotmail.com>
2015-09-30 12:37:41 +0200

    upt

--- OOMULab1/src/grupp4/calculator/view/ConsoleView.java
+++ OOMULab1/src/grupp4/calculator/view/ConsoleView.java
@@ -5,9 +5,35 @@
  */
 package grupp4.calculator.view;
 
+
+import grupp4.calculator.exeptions.*;
+import grupp4.calculator.model.RPNCalculator;
+import java.util.Scanner;
+
 /**
  *
  * @author alexander
  */
-public class ConsoleView {
+public class ConsoleView implements ViewInterface{
+
+    @Override
+    public void RenameThisFunction(String[] args)  throws InvalidTokenException, DivideByZeroException{
+        RPNCalculator c = new RPNCalculator();
+        String result;
+        
+        while(true){
+            System.out.print("Ange RPN uttryck <retur> (tom sträng = avsluta):");
+            Scanner scan = new Scanner(System.in); 
+            String str =scan.nextLine();
+            if (str.isEmpty() == true){
+                System.out.println("Användaren avslutade applikationen\n");
+                System.exit(0);
+            }
+            else{
+                result = c.calc(str);
+                System.out.println("Resultat: "+ result);
+            }   
+        }
+    }
 }
+
--- OOMULab1/src/grupp4/calculator/view/FileView.java
+++ OOMULab1/src/grupp4/calculator/view/FileView.java
@@ -5,10 +5,40 @@
  */
 package grupp4.calculator.view;
 
+import grupp4.calculator.exeptions.*;
+import grupp4.calculator.model.RPNCalculator;
+import java.io.*;
+
 /**
  *
  * @author alexander
  */
-public class FileView {
-    
+public class FileView implements ViewInterface{
+
+    @Override
+    public void RenameThisFunction(String[] args) throws InvalidTokenException, DivideByZeroException{
+        
+        try {
+            BufferedReader fil= new BufferedReader(new FileReader(args[0]));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(args[1]));
+            RPNCalculator c = new RPNCalculator();
+            String line, result;
+            
+            System.out.println("Beräknar RPN uttryck från " + args[0] + "\nSkriver svaren till " + args[1]);
+            while((line = fil.readLine()) != null){
+             
+            result = c.calc(line);
+            
+            writer.write(result);
+            writer.newLine();
+            }
+            writer.close();
+            System.out.println("Beräkningen är klar");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+	}
+        
+    }
+     
 }
+    
